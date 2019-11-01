919fcfe1af78dc777b96a075cb0352891ad5e693
Hithun-Harindran <hithun.harindran@invenio-solutions.com>
2019-11-01 11:34:48 +0000

    save

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("practice.Chart_assignment.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"practice/Chart_assignment/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
